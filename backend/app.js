bf327378bcb618a8936022fd3c4fc73260a33e55
Bhoomika <bhoomikahm18@gmail.com>
2024-01-11 12:31:38 +0530

    Installing cookie-parser package

--- backend/app.js
+++ backend/app.js
@@ -3,9 +3,11 @@ const mongoose = require('mongoose');
 const router = require("./routes/user_routes.js");
 const dotenv = require('dotenv');
 
+const cookieParser = require('cookie-parser');
 dotenv.config({path: './.env'});
 
 const app = express();
+app.use(cookieParser());
 app.use(express.json());
 app.use('/api', router);
 
