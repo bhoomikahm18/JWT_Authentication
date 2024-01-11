c89759fa6eb0b53c1cfaeadae1ba0fb72fcf5c29
Bhoomika <bhoomikahm18@gmail.com>
2024-01-11 12:51:08 +0530

    Creating components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,24 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header.jsx';
+import Login from './components/Login.jsx';
+import Signup from './components/Signup.jsx';
+import Welcome from './components/Welcome.jsx';
 
 function App() {
   return (
-    <div className="App">
-      Hello World
-    </div>
+    <>
+      <header>
+        <Header />
+      </header>
+      <main>
+        <Routes>
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<Signup />} />
+          <Route path='/user' element={<Welcome />} />
+        </Routes>
+      </main>
+    </>
   );
 }
 
