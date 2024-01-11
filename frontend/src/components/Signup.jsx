b73dbbd33ed5104748d417a9ed26b1a396a249c5
Bhoomika <bhoomikahm18@gmail.com>
2024-01-11 13:00:35 +0530

    Designing signup page

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
 
 function Signup() {
+  function handleSubmit() {
+
+  }
   return (
-    <div>Signup</div>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <Box marginLeft={'auto'} marginRight={'auto'} width={300} display={'flex'} flexDirection={'column'} justifyContent={'center'} alignItems={'center'}>
+          <Typography variant='h2'>Signup</Typography>
+          <TextField variant='outlined' placeholder='Name' margin='normal' />
+          <TextField variant='outlined' placeholder='Email' margin='normal' />
+          <TextField variant='outlined' placeholder='Password' margin='normal' />
+          <Button variant='contained' type='submit'>Signup</Button>
+        </Box>
+      </form>
+    </div>
   )
 }
 
